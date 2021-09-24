2405515cc77f0439261af1b8a7b5e34f3f5fe594
Samuel Chen <schen30@stuy.edu>
2021-09-23 20:27:04 -0400

    answered the questions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,9 @@
+//parameters should be 0 and 32 for celsius and fahrenheit respectively as 0 celsius should be equal to 32 fahrenheit
+//parameters should also be -40 as both celsius and fahrenheit are equal at -40
+//the type of parameter should be integers and the functions should return integers 
+
+
+
 public class CtoFTester{
   public static void main(String[]args){
     celsiusToFahrenheit(0);
