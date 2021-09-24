2d644b506ed1c6f0b05a9cf31674df210b54c943
Samuel Chen <schen30@stuy.edu>
2021-09-23 20:18:05 -0400

    added fahrenheit to celsius

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,8 +2,13 @@ public class CtoFTester{
   public static void main(String[]args){
     celsiusToFahrenheit(0);
     celsiusToFahrenheit(-40);
+    fahrenheitToCelsius(32);
+    fahrenheitToCelsius(-40);
   }
   public static void celsiusToFahrenheit(int C){
     System.out.println(C * 9 / 5 +32);
+  }
+  public static void fahrenheitToCelsius(int F){
+    System.out.println((F -32) *5 / 9);
   }
 }
